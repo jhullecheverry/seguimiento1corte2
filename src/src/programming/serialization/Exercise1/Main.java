37df7318bdffd9ad169a016b1356f7df8dfd3e6f
jhullecheverry <131423312+jhullecheverry@users.noreply.github.com>
2023-09-19 23:10:07 -0500

    commit

---
+++ src/src/programming/serialization/Exercise1/Main.java
@@ -0,0 +1,8 @@
+package src.programming.serialization.Exercise1;
+
+public class Main {
+    public static void main(String[] args) {
+        Cajero cajero = new Cajero();
+        cajero.iniciarSesion();
+    }
+}
